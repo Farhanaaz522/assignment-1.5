f49b9f032bbb07d723b0d0efcb1c0349649d3bce
Farhanaaz522 <37798788+Farhanaaz522@users.noreply.github.com>
2018-05-21 22:03:57 +0530

    Delete assignment 2.5.2hist.R

--- assignment 2.5.2hist.R
+++
@@ -1,5 +0,0 @@
-#Question 2
-#Prepare a histogram of the number of characters in each US state.
-# histogram of characters
-hist(nchar(states), main = "Histogram",
-     xlab = "number of characters in US State names")
