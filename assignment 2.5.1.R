7beabe343ad8a36eedfa05a316ea28901a29c4f5
Farhanaaz522 <37798788+Farhanaaz522@users.noreply.github.com>
2018-05-21 22:03:38 +0530

    Delete assignment 2.5.1.R

--- assignment 2.5.1.R
+++
@@ -1,9 +0,0 @@
-#Question 1:
-#states = rownames(Us Arrests)
-# get states names with 'w'.
-# get states names with 'W'.
-states=rownames(USArrests)
-# get states names with 'w'
-grep(pattern = "w", x = states, value = TRUE)
-# get states names with 'W'
-grep(pattern = "W", x = states, value = TRUE)
